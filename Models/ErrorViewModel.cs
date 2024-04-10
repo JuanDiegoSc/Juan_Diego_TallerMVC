9b45fa250d3fbb64c3c682e0c572dbd851b8eb6d
laboratorio <laboratorio@UPOAULA10303>
2024-04-10 10:42:45 -0500

    Agregar archivos de proyecto.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Juan_Diego_TallerMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
